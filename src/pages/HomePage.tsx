b402f4a67d9c830c2b19354b55467c70ba84e06d
tokkissi <alal4674@gmail.com>
2023-08-05 03:55:29 +0900

    feat: add HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-full bg-gray-300">
+      <img src="/home-image.jpg" alt="프리온보딩 사전과제" />
+    </div>
+  );
+}
