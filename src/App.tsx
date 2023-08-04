3bd7b2cc9bb92716becd22945b641cfdb8f7b16a
tokkissi <alal4674@gmail.com>
2023-08-05 05:17:52 +0900

    feat: add routing based on accesstoken

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from "react";
+import { BrowserRouter, Navigate, useRoutes } from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
+import SigninPage from "./pages/SigninPage";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import TodoPage from "./pages/TodoPage";
+import { AuthProvider, AuthContext } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routing />
+      </AuthProvider>
+    </BrowserRouter>
   );
+}
+
+function Routing() {
+  const { accessToken } = useContext(AuthContext);
+
+  const routing = useRoutes([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <HomePage />, index: true },
+        {
+          path: "signup",
+          element: accessToken ? (
+            <Navigate replace to="/todo" />
+          ) : (
+            <SignupPage />
+          ),
+        },
+        {
+          path: "signin",
+          element: accessToken ? (
+            <Navigate replace to="/todo" />
+          ) : (
+            <SigninPage />
+          ),
+        },
+        {
+          path: "todo",
+          element: accessToken ? (
+            <TodoPage />
+          ) : (
+            <Navigate replace to="/signin" />
+          ),
+        },
+      ],
+    },
+  ]);
+
+  return routing;
 }
 
 export default App;
