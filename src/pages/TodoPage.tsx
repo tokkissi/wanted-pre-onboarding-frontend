f4b57c1599c206de3abfb713fa0c180118e2a0bd
tokkissi <alal4674@gmail.com>
2023-08-05 03:56:23 +0900

    feat: add TodoPage

---
+++ src/pages/TodoPage.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function TodoPage() {
+  return (
+    <div>
+      <h1>TODO LIST</h1>
+    </div>
+  );
+}
